9b904053c61f24bd30bd5ad3b1c7add76f458c77
raghu <raghumina11@gmail.com>
2020-06-19 18:57:33 +0530

    Create Practice.py

---
+++ Practice.py
@@ -0,0 +1,21 @@
+# In this file i am solving  the python problems
+# i am practicing a lot of python problems
+# problems may be or may not be related to data Science
+
+
+# Problem 1
+# Best cellular plan
+# Find the most economical cellular plan in terms of cheapest per minute calling cost
+#
+# Instructions
+# We have provided you with details of 3 plans available to you.
+#
+# Divide no. of minutes by the price for each plan and save the result in cpm_1, cpm_2 and cpm_3 respectively
+# (Meaning divide price_1 by nom_1 and store the result in cpm_1, so on and so forth)
+#
+# Print cpm_1, cpm_2 and cpm_3
+#
+# Write the following line of code to find the minimum amongst the variables calculated above
+#
+# economical_rate= min(cpm_1,cpm_2,cpm_3)
+
