8550c1a8ac1e362c9a3af986c48e1bb921ffca28
raghu <raghumina11@gmail.com>
2020-06-30 22:26:45 +0530

    Update Practice.py

--- Practice.py
+++ Practice.py
@@ -199,3 +199,10 @@ print(interest)
 # Second element of split should be "Python".
 
 #
+name  = "Monty"
+title = "Python"
+full_name = name + title
+print(full_name)
+first_name = full_name[0:5]
+print(first_name)
+
