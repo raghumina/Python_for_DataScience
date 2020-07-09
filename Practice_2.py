a453db50adfe07fa33116d90c01b5cf615b15b6f
raghu <raghumina11@gmail.com>
2020-07-09 22:14:17 +0530

    Update Practice_2.py

--- Practice_2.py
+++ Practice_2.py
@@ -1,3 +1,20 @@
+
+# initial list
+scores = [80,90,95,100]
+
+# memory location of initial list
+print('Memory loaction initially is ' , id(scores))
+print('='*20)
+# change value
+scores[2] = 99
+# display modified list
+print(scores)
+print('='*20)
+# check memory loaction of modified list
+print('Memory location of modified list is ' , id(scores))
+
+
+
 # Problems related to list etc
 # more typical or data related problems
 
@@ -17,5 +34,4 @@
 #
 # Calculate the mean of their weights using the formula sum / number of elements and save it to a variable mean.
 # Print this out too.
-
 
