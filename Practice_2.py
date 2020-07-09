0f4327217563c3c22bb21c1d874f1928da368dc6
raghu <raghumina11@gmail.com>
2020-07-09 22:09:48 +0530

    Create Practice_2.py

---
+++ Practice_2.py
@@ -0,0 +1,21 @@
+# Problems related to list etc
+# more typical or data related problems
+
+# In this task, you will store the weights of a family of a four weighing of 70, 80, 45 and 50 kg respectively
+# in a list and perform do some operations on it to calculate the mean of the weights.
+#
+# Instructions
+# Store the values in a list called weights.
+#
+# Save the maximum weight from the list using the max() method to a maximum variable. Print it out.
+#
+# Suddenly, you realize that you have wrongly entered 45 and that should be 48. So,
+# it's time to replace the wrong weight with the correct one. Since 45 is in the second index,
+# use weights[2] = 48 to replace the value. Display the weights using print()
+#
+# Now calculate the sum of the weights using the sum() function and save it to a total variable. Print it out.
+#
+# Calculate the mean of their weights using the formula sum / number of elements and save it to a variable mean.
+# Print this out too.
+
+
