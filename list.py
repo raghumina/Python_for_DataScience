94b019fbfbb48e02cbdfcbac5fb8d3f996760218
raghu <raghumina11@gmail.com>
2020-08-10 22:39:37 +0530

    Update list.py

--- list.py
+++ list.py
@@ -11,3 +11,10 @@ print(scores)
 print('='*20)
 # check memory loaction of modified list
 print('Memory location of modified list is ' , id(scores))
+
+
+
+area = { 'living' : [400,450], 'bedroom' : [650, 800], 'kitchen' : [300, 250], 'garage': [250, 0]}
+print(area.get('living'))
+print('='*50)
+print (area.get('color','This key is absent' ))
