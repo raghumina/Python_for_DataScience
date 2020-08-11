e6f68993c0f7556caf934c9f1872a467e76f9a8b
raghu <raghumina11@gmail.com>
2020-08-11 22:48:46 +0530

    Create Tuples.py

---
+++ Tuples.py
@@ -0,0 +1,18 @@
+#
+#
+# Now you will use tuples on the same set of weights of a four-membered family having weights of 70, 80, 45,
+# and 50 kg respectively, and perform some operations on it to calculate the mean of the weights.
+#
+# Instructions
+# Suddenly you realize that you have wrongly entered 45 and that it should be 48.
+# Could that be done after storing the given weights inside a tuple?
+# Since it cannot be done, create a tuple with the correct weights.
+# Name this new tuple weights_new and perform the following operations on it.
+# Calculate the maximum and minimum weights using the max() and min() functions respectively on the new weights_new tuple.
+# Save them to the maximum and minimum variables.
+# Now calculate the sum of the weights using the sum() function and save it to sum_weights.
+# Save the mean of their weights (using the formula sum / number of elements) to a mean_new variable.
+# Test Cases: The value of maximum should be 80.
+# The value of minimum should be 48.
+# The calculated value of sum_weights should be 248.
+# The calculated value of mean_new should be 62.0.
