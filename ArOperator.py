8659d3b0c412686d0483218dab83973d9449bc4a
raghu <raghumina11@gmail.com>
2020-08-12 22:29:33 +0530

    Create ArOperator.py

---
+++ ArOperator.py
@@ -0,0 +1,19 @@
+#
+#
+# In this task, you will be performing some mathematical calculations with the help of operators.
+# You are given two numbers x = 15 and y = 4.
+#
+# Instructions
+# Perform addition. Save it to an addition variable and print it out.
+#
+# Perform subtraction. Save it to a subtraction variable and print out the result.
+#
+# Perform multiplication. Save it to a multiplication variable and print out the result.
+#
+# Perform division. Save it to a division variable and print out the result.
+#
+# Perform integer division. Save it to an integer_division variable and print out the result
+#
+# Perform exponentiation (x^yx
+# y
+#  ), Save it to an exponent variable and print out the result.
